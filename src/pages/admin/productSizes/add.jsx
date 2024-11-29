6f58a63260f7097fa61f83b87f81b1841e541b89
Renan de Carvalho Ribeiro <renancr176@gmail.com>
2024-11-29 19:28:25 -0300

    Criado crud de tamanho de produtos.

--- src/pages/admin/productSizes/add.jsx
+++ src/pages/admin/productSizes/add.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import useAlert from "../../../hooks/alert";
+import { Container } from "react-bootstrap";
+import useProductSizeService from "../../../services/productSizeService";
+import AdminProductSizeForm from "./form";
 
 export default function AdminAddProductSize() {
+  const { fireSuccess, fireRequestError } = useAlert();
+  const { createProductSize } = useProductSizeService();
+  const navigate = useNavigate();
+  const { t } = useTranslation("adminProductSize", { keyPrefix: "add" });
+
+  const handleSubmit = (values) => {
+    return createProductSize(values)
+      .then(({ data }) => {
+        fireSuccess(t("success")).then(() => {
+          navigate(`../edit/${data.id}`);
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+        fireRequestError(err);
+      });
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <Container fluid>
+      <h3>{t("title")}</h3>
+      <AdminProductSizeForm onSubmit={handleSubmit} />
+    </Container>
+  );
 }
--- src/pages/admin/productSizes/edit.jsx
+++ src/pages/admin/productSizes/edit.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import useAlert from "../../../hooks/alert";
+import { Container } from "react-bootstrap";
+import useProductSizeService from "../../../services/productSizeService";
+import AdminProductSizeForm from "./form";
+import Spinner from "../../../components/elements/spinner";
 
 export default function AdminEditProductSize() {
+  const { id } = useParams();
+  const { fireSuccess, fireRequestError, fireConfirm } = useAlert();
+  const { getProductSizeById, updateProductSize } = useProductSizeService();
+  const navigate = useNavigate();
+  const { t } = useTranslation("adminBrand", { keyPrefix: "edit" });
+  const [data, setData] = useState();
+
+  useEffect(() => {
+		if (!id) return navigate("../");
+
+		getProductSizeById(id)
+			.then(({ data }) => {
+        setData(data);
+			})
+			.catch((err) => {
+				console.error(err);
+				fireRequestError(err)
+        .then(() => {
+          navigate("../");
+        });
+			});
+	}, [id]);
+
+  const handleSubmit = async (values) => {
+    if (!await fireConfirm(t('confirm')))
+      return;
+
+    return updateProductSize(values)
+      .then(({ data }) => {
+        fireSuccess(t("success"));
+      })
+      .catch((err) => {
+        console.error(err);
+        fireRequestError(err);
+      });
+  };
+
   return (
-    <div>
-      
-    </div>
+    <Container fluid>
+      <h3>{t("title")}</h3>
+      {data ? (
+        <AdminProductSizeForm onSubmit={handleSubmit} data={data} />
+      ): (
+        <div style={{ width: "100%", textAlign: "center" }}>
+					<Spinner />
+				</div>
+      )}
+    </Container>
   )
 }
