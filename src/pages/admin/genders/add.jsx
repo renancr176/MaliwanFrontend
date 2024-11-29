d2fe27da3d7c6f8f96d577e44872c8dfd9d26737
Renan de Carvalho Ribeiro <renancr176@gmail.com>
2024-11-29 11:14:23 -0300

    Criado crud de generos

--- src/pages/admin/genders/add.jsx
+++ src/pages/admin/genders/add.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import useAlert from "../../../hooks/alert";
+import { Container } from "react-bootstrap";
+import useGenderService from "../../../services/genderService";
+import AdminGenderForm from "./form";
 
 export default function AdminAddGender() {
+  const { fireSuccess, fireRequestError } = useAlert();
+  const { createGender } = useGenderService();
+  const navigate = useNavigate();
+  const { t } = useTranslation("adminGender", { keyPrefix: "add" });
+
+  const handleSubmit = (values) => {
+    return createGender(values)
+      .then(({ data }) => {
+        fireSuccess(t("success")).then(() => {
+          navigate(`../edit/${data.id}`);
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+        fireRequestError(err);
+      });
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <Container fluid>
+      <h3>{t("title")}</h3>
+      <AdminGenderForm onSubmit={handleSubmit} />
+    </Container>
+  );
 }
--- src/pages/admin/genders/form.jsx
+++ src/pages/admin/genders/form.jsx
@@ -1,9 +1,114 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { Formik } from "formik";
+import * as yup from "yup";
+import { Row, Col, Form, ButtonGroup, Button } from "react-bootstrap";
+import { FaSpinner, FaPlus, FaRegEdit } from "react-icons/fa";
+import { IoReturnUpBackOutline } from "react-icons/io5";
 
-export default function AdminGenderForm() {
+export default function AdminGenderForm({
+  onSubmit,
+  data = {
+    name: "",
+    sku: "",
+  },
+}) {
+  const { t } = useTranslation("adminGender", { keyPrefix: "form" });
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmitThis = (values) => {
+    setSubmitting(true);
+    let data = { ...values };
+    onSubmit(data).finally(() => setSubmitting(false));
+  };
+
+  const schema = yup.object().shape({
+    name: yup.string().required().min(5).max(255),
+    sku: yup.string().required().min(1).max(5),
+  });
+
   return (
-    <div>
-      
-    </div>
-  )
+    <Formik
+      enableReinitialize={true}
+      validationSchema={schema}
+      onSubmit={handleSubmitThis}
+      initialValues={data}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        values,
+        touched,
+        errors,
+      }) => (
+        <Form noValidate onSubmit={handleSubmit}>
+          <Row>
+            <Form.Group as={Col} className="mb-2">
+              <Form.Label column md={3}>
+                <span className="text-danger">*</span>
+                {t("fieldNameLabel")}
+              </Form.Label>
+              <Col>
+                <Form.Control
+                  type="text"
+                  name="name"
+                  maxLength={255}
+                  value={values.name}
+                  onChange={handleChange}
+                  isValid={touched.name && !errors.name}
+                  isInvalid={touched.name && errors.name}
+                />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Col} className="mb-2">
+              <Form.Label column md={3}>
+                <span className="text-danger">*</span>
+                {t("fieldSkuLabel")}
+              </Form.Label>
+              <Col>
+                <Form.Control
+                  type="text"
+                  name="sku"
+                  maxLength={5}
+                  value={values.sku}
+                  onChange={(e) => {
+                    e.target.value = e.target.value.toUpperCase();
+                    handleChange(e);
+                  }}
+                  isValid={touched.name && !errors.name}
+                  isInvalid={touched.name && errors.name}
+                />
+              </Col>
+            </Form.Group>
+          </Row>
+          <Row className="mt-4">
+            <Col className="d-grid gap-2">
+              <ButtonGroup>
+                <Link 
+                  className="btn btn-secondary"
+                  to={"../"}>
+                  <IoReturnUpBackOutline />
+                  {" "}{t(`labelBtnGoBack`)}
+                </Link> 
+                <Button
+                  type="submit"
+                  variant={!data?.id ? "success" : "warning"}
+                >
+                  {submitting ? (
+                    <FaSpinner className="animate-spin" />
+                  ) : !data?.id ? (
+                    <FaPlus />
+                  ) : (
+                    <FaRegEdit />
+                  )}{" "}
+                  {t(`labelBtnSubmit.${!data?.id ? "add" : "edit"}`)}
+                </Button>
+              </ButtonGroup>
+            </Col>
+          </Row>
+        </Form>
+      )}
+    </Formik>
+  );
 }
