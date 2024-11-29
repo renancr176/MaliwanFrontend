7ac4c4610402341c2972582f8369b951b94ca7f1
Renan de Carvalho Ribeiro <renancr176@gmail.com>
2024-11-29 11:32:52 -0300

    Criado crud de forma de pagamento

--- src/pages/admin/paymentMethods/edit.jsx
+++ src/pages/admin/paymentMethods/edit.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import useAlert from "../../../hooks/alert";
+import { Container } from "react-bootstrap";
+import usePaymentMethodService from "../../../services/paymentMethodService";
+import AdminBrandForm from "./form";
+import Spinner from "../../../components/elements/spinner";
 
 export default function AdminEditPaymentMethod() {
+  const { id } = useParams();
+  const { fireSuccess, fireRequestError, fireConfirm } = useAlert();
+  const { getPaymentMethodById, updatePaymentMethod } = usePaymentMethodService();
+  const navigate = useNavigate();
+  const { t } = useTranslation("adminPaymentMethod", { keyPrefix: "edit" });
+  const [data, setData] = useState();
+
+  useEffect(() => {
+		if (!id) return navigate("../");
+
+		getPaymentMethodById(id)
+			.then(({ data }) => {
+        setData(data);
+			})
+			.catch((err) => {
+				console.error(err);
+				fireRequestError(err)
+        .then(() => {
+          navigate("../");
+        });
+			});
+	}, [id]);
+
+  const handleSubmit = async (values) => {
+    if (!await fireConfirm(t('confirm')))
+      return;
+
+    return updatePaymentMethod(values)
+      .then(({ data }) => {
+        fireSuccess(t("success"));
+      })
+      .catch((err) => {
+        console.error(err);
+        fireRequestError(err);
+      });
+  };
+
   return (
-    <div>
-      
-    </div>
+    <Container fluid>
+      <h3>{t("title")}</h3>
+      {data ? (
+        <AdminBrandForm onSubmit={handleSubmit} data={data} />
+      ): (
+        <div style={{ width: "100%", textAlign: "center" }}>
+					<Spinner />
+				</div>
+      )}
+    </Container>
   )
 }
--- src/pages/admin/paymentMethods/index.jsx
+++ src/pages/admin/paymentMethods/index.jsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import React from "react";
+import { useTranslation } from "react-i18next";
+import Spinner from "../../../components/elements/spinner";
+import PaginationMenu from "../../../components/paginationMenu";
+import usePagination from "../../../hooks/pagination";
+import {
+  Container,
+  Table,
+  Col,
+  Row,
+  Badge,
+  ButtonGroup,
+} from "react-bootstrap";
+import { FaRegEdit } from "react-icons/fa";
+import { USER_ROLES } from "../../../utils/userRoles";
+import { useAuth } from "../../../hooks/auth";
+import usePaymentMethodService from "../../../services/paymentMethodService";
+import AdminBrandFilters from "./filter";
+import LinkTooltip from "../../../components/linkTooltip";
 
 export default function AdminPaymentMethods() {
+  const { t } = useTranslation("adminPaymentMethod", { keyPrefix: "index" });
+  const { searchPaymentMethods } = usePaymentMethodService();
+  const { hasRoles } = useAuth();
+  const {
+    data: pageData,
+    pageIndex,
+    totalPages,
+    nextPage,
+    prevPage,
+    setPage,
+    isLoading,
+    setFilter,
+  } = usePagination(searchPaymentMethods);
+
   return (
-    <div>
-      
-    </div>
-  )
+    <Container fluid>
+      <Row>
+        <AdminBrandFilters setFilter={setFilter} isLoading={isLoading}/>
+      </Row>
+      <hr/>
+      <Row>
+        <Col>
+          <Table responsive="sm" striped>
+            <thead>
+              <tr>
+                <th style={{ width: "10%" }}>#</th>
+                <th>{t("table.header.name")}</th>
+                <th style={{ width: "10%" }}>{t("table.header.active")}</th>
+                <th style={{ width: "10%" }}>{t("table.header.actions")}</th>
+              </tr>
+            </thead>
+            <tbody>
+              {isLoading ? (
+                <Spinner />
+              ) : (
+                pageData?.map((item) => (
+                  <tr key={item.id}>
+                    <td className="fw-bold">{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>
+                      <Badge bg={item.active ? "success" : "danger"}>
+                        {t(`table.body.active.${item.active}`)}
+                      </Badge>
+                    </td>
+                    <td>
+                      {hasRoles([USER_ROLES.ADMIN]) && (
+                        <ButtonGroup>
+                          <LinkTooltip
+                            variant="warning"
+                            placement="left"
+                            title={t("labelBtnEdit")}
+                            to={`edit/${item.id}`}
+                            className="btn btn-warning"
+                          >
+                            <FaRegEdit />
+                          </LinkTooltip>
+                        </ButtonGroup>
+                      )}
+                    </td>
+                  </tr>
+                ))
+              )}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="d-flex justify-content-center">
+          <PaginationMenu
+            nPagesToShow={5}
+            {...{ pageIndex, totalPages, nextPage, prevPage, setPage }}
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
